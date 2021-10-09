4bb0dd16480b500d36e1cd61197382014cd2fb70
vincemilo <vincemilo@gmail.com>
2021-10-09 10:21:38 -0600

    All seem to be working though may need refactor

--- app.rb
+++ app.rb
@@ -56,22 +56,75 @@ class LinkedList
     @tail = temp
   end
 
-  # def contains?(value)
-  #   @arr.any?(value)
-  # end
+  def contains?(value)
+    temp = @head
+    until temp.nil?
+      return true if temp.value == value
 
-#   def find(value)
-#     @arr.index(value)
-#   end
+      temp = temp.next_node
+    end
+    false
+  end
 
-  class Node
-    attr_accessor :value, :next_node
-    def initialize(value = nil, next_node = nil)
-      @value = value
-      @next_node = next_node
+  def find(value)
+    temp = @head
+    count = 0
+    until temp.nil?
+      return count if temp.value == value
+
+      count += 1
+      temp = temp.next_node
+    end
+    nil
+  end
+
+  def to_s
+    temp = @head
+    string = ''
+    until temp.nil?
+      string += "( #{temp.value} ) -> "
+      temp = temp.next_node
+    end
+    string += 'nil'
+    string
+  end
+
+  def insert_at(value, index)
+    new_node = Node.new(value)
+    temp = @head
+    count = 0
+    until temp.nil?
+      return prepend(value) if index.zero?
+
+      if count == index - 1
+        new_node.next_node = temp.next_node
+        temp.next_node = new_node
+      end
+      count += 1
+      temp = temp.next_node
     end
   end
 
+  def remove_at(index)
+    temp = @head
+    count = 0
+    until temp.nil?
+      return @head = temp.next_node if index.zero?
+
+      temp.next_node = temp.next_node.next_node if count == index - 1
+      count += 1
+      temp = temp.next_node
+    end
+  end
+end
+
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
 end
 
 list = LinkedList.new
@@ -84,10 +137,16 @@ list.prepend(4)
 # p list.tail
 # p list
 # p list.size
-#p list
+# p list
 p list.at(5)
 # p list.head.next_node
 # p list.tail.next_node
 p list.tail
 p list.pop
-p list
+p list.contains?(1)
+p list.find(2)
+puts list.to_s
+list.insert_at('foo', 2)
+puts list.to_s
+list.remove_at(1)
+puts list.to_s
